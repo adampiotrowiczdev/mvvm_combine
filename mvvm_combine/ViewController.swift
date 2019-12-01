cfb29e5820f58159b63045b81abec070202a2067
a.piotrowicz <a.piotrowicz@synermedia.pl>
2019-12-01 19:23:55 +0100

    Project init

---
+++ mvvm_combine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mvvm_combine
+//
+//  Created by Syner Media on 01/12/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ mvvm_combineTests/mvvm_combineTests.swift
@@ -0,0 +1,33 @@
+//
+//  mvvm_combineTests.swift
+//  mvvm_combineTests
+//
+//  Created by Syner Media on 01/12/2019.
+//
+
+import XCTest
+@testable import mvvm_combine
+
+class mvvm_combineTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
