0aaccf661b051b0c49277b0bb0f48e46866d1fa5
a.piotrowicz <a.piotrowicz@synermedia.pl>
2019-12-01 20:30:22 +0100

    carthage init

---
+++ mvvm_combine/ViewModels/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  mvvm_combine
+//
+//  Created by Syner Media on 01/12/2019.
+//
+
+import Foundation
---
+++ mvvm_combine/ViewModels/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  mvvm_combine
+//
+//  Created by Syner Media on 01/12/2019.
+//
+
+import Foundation
