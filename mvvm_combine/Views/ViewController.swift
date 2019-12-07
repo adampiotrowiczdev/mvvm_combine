7e3a1a364d0fa31889914b01fa1965e254e972c3
jaand <45097902+jaand@users.noreply.github.com>
2019-12-07 22:54:36 +0100

    Beer picker implemented (#3)
    * Beer picker implemented
    
    * Beer Picker first fix
    
    * Beer picker second fix-constraints + disappear btn
    
    remake of text field's placement using constraints, fixed view.endEditting (changed to true)
    
    * got rid of Magic Numbers
    
    * Consts added
    
    * Toast class and FactoryView

--- mvvm_combine/Views/ViewController.swift
+++ mvvm_combine/Views/ViewController.swift
@@ -9,53 +9,102 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
-
-    private let button = UIButton()
+    
+    private struct Consts {
+        static let beerArray = ["Komes","Tyskie","Łomża","Żywiec","Birra Moretti"]
+        static let numberOfColumnsInPicker = 1
+        static let beerTextFieldFontSize : CGFloat = 20
+        static let beetTextFieldTopOffset : CGFloat = 50
+        static let toastShowingTime : Double = 50
+    }
+    
+    private let drinkButton = FactoryView.drinkButton
+    private let beerPicker = FactoryView.beerPicker
+    private let beerTextField = FactoryView.beerTextField
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = R.color.tomato()
-        
-        button.setTitle("BEER", for: .normal)
-        
-        view.addSubview(button)
-        
-        button.snp.makeConstraints {
-            $0.center.equalTo(view)            
+        addSubviews()
+        setUpConstraints()
+        configureViews()
+    }
+    
+    fileprivate func addSubviews() {
+        view.addSubview(drinkButton)
+        view.addSubview(beerTextField)
+    }
+    
+    fileprivate func setUpConstraints() {
+        beerTextField.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(Consts.beetTextFieldTopOffset)
+            $0.width.equalTo(view)
         }
         
-        button.addTarget(self, action:#selector(beerTapped), for: .touchUpInside)
-        
-        // Do any additional setup after loading the view.
+        drinkButton.snp.makeConstraints {
+            $0.center.equalTo(view)
+        }
+    }
+    
+    fileprivate func configureViews() {
+        view.backgroundColor = R.color.tomato()
+        beerPicker.dataSource = self
+        beerPicker.delegate = self
+        beerTextField.inputView = beerPicker
+        drinkButton.addTarget(self, action:#selector(beerTapped), for: .touchUpInside)
     }
     
     @objc func beerTapped(_ sender : UIButton) {
-        self.showToast(controller: self, message: "CHEERS", seconds: 1)
+        Toast().show(controller: self, message: "CHEERS", seconds: Consts.toastShowingTime)
     }
     
-    func showToast(controller: UIViewController, message : String, seconds: Double) {
-        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-        alert.view.backgroundColor = UIColor.black
-        alert.view.alpha = 0.6
-        alert.view.layer.cornerRadius = 15
-
-        controller.present(alert, animated: true)
-
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + seconds) {
-            alert.dismiss(animated: true)
+    private struct FactoryView {
+        static var drinkButton : UIButton {
+            let button = UIButton()
+            button.setTitle("DRINK", for: .normal)
+            return button
+        }
+        
+        static var beerPicker : UIPickerView {
+            let picker = UIPickerView()
+            return picker
+        }
+        
+        static var beerTextField: UITextField {
+            let textField = UITextField()
+            textField.tintColor = .clear
+            textField.backgroundColor = R.color.tomato()
+            textField.borderStyle = .roundedRect
+            textField.font = UIFont.systemFont(ofSize: Consts.beerTextFieldFontSize)
+            textField.textAlignment = .center
+            textField.textColor = .white
+            textField.attributedPlaceholder = NSAttributedString(string: "Choose your taste here",
+            attributes:[NSAttributedString.Key.foregroundColor: UIColor.white])
+            return textField
         }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return Consts.numberOfColumnsInPicker
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return Consts.beerArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int , forComponent component: Int) -> String? {
+        return Consts.beerArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int , inComponent component: Int) {
+        beerTextField.text = Consts.beerArray[row]
+        self.view.endEditing(true)
+    }
+}
+
+
+
+
